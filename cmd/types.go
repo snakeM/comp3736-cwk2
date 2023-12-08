33cf56f43a25a57e22ecf27b55982e791aae5ad2
Matt Auger <sc20mra@leeds.ac.uk>
2023-12-08 16:49:46 +0000

    Remove unnecessary types

--- cmd/types.go
+++ cmd/types.go
@@ -9,24 +9,10 @@ type ResultRecord struct {
 	Answer string `json:"answer"`
 }
 
-type LineChart struct {
-	Label string `json:"label"`
-	Data []int `json:"data"`
-	Fill bool `json:"fill"`
-}
-
-type LineData struct {
-	Labels []string `json:"labels"`
-	Datasets []LineChart `json:"datasets"`
-}
-
 type RequestData struct {
 	Data []ResultRecord `json:"data"`
 }
 
-type Charts struct {
-	LineCharts []LineData `json:"lineCharts"`
-}
 
 type chartDataset struct {
 	Label string `json:"label"`
