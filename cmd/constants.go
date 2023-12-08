13ca4456a31272b785d9ad2391118456d435ede6
Matt Auger <sc20mra@leeds.ac.uk>
2023-12-08 18:14:02 +0000

    Fix constants.go

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,4 +1,4 @@
 package main
 
-const OLYMPIC_YEARS = []string{"1972", "1976", "1980", "1984", "1988", "1992", "1996", "2000", "2004", "2008", "2012", "2016"}
-const COUNTRIES = []string{"USA", "Belgium", "Great Britain", "Spain", "Italy", "France", "Greece", "Japan"}
+var OLYMPIC_YEARS []string = []string{"1972", "1976", "1980", "1984", "1988", "1992", "1996", "2000", "2004", "2008", "2012", "2016"}
+var COUNTRIES []string = []string{"USA", "Belgium", "Great Britain", "Spain", "Italy", "France", "Greece", "Japan"}
